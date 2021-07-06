93802e8d10ade6d448b0f458a5fcff8edcbcd5d9
jauhhari <ervanadiwijaya@gmail.com>
2021-07-06 22:51:56 +0700

    ok

---
+++ soal1.php
@@ -0,0 +1,13 @@
+<?php
+
+$gap = 360;
+$speed = 90;
+
+$profit = 300;
+
+$time = $gap * $inMinute / $speed;
+
+echo "Jarak kota A - Z = 360 km </br>";
+echo "Kecepatan motor = 90 km/h </br>";
+echo "Waktu tempuh = ? </br>";
+echo "Jawaban : ${time} menit";
---
+++ soal2.php
@@ -0,0 +1,12 @@
+<?php
+
+$save = 150000;
+$bunga = 12.5 / 100;
+
+$totMoney = ($save * $bunga) + $save;
+
+
+echo "Tabungan Awal = Rp ${save} </br>";
+echo "Bunga Setahun = 12.5 % </br>";
+echo "Total Tabungan Setahun = ? </br>";
+echo "Jawaban : Rp ${totMoney}";
